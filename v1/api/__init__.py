6a9675138abcaed4f9dacf1c0ace81701f80df90
Robus Gauli <robusgauli@gmail.com>
2016-12-17 21:54:48 +0545

    host

--- v1/api/__init__.py
+++ v1/api/__init__.py
@@ -3,7 +3,8 @@ from sqlalchemy.orm import sessionmaker
 from api.models.model import init_db
 #global variables
 app = Flask(__name__)
-URL_DB = 'postgres://postgres:robus@localhost:5432/restaurantv1'
+#URL_DB = 'postgres://postgres:robus@localhost:5432/restaurantv1'
+URL_DB = 'postgres://postgres:robus@139.59.25.134:5432/restaurantv1'
 engine = init_db(URL_DB)
 Session = sessionmaker(bind=engine)
 
