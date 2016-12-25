6e2cb94f746c357df8d1ffb61091614909ea2a35
Robus Gauli <robusgauli@gmail.com>
2016-12-25 16:13:12 +0545

    bills

--- v1/api/billendpoints.py
+++ v1/api/billendpoints.py
@@ -75,34 +75,74 @@ def setBill():
 
 
 @app.route('/api/v1/bills/<int:b_id>', methods=['GET'])
-def getBills(b_id):
+def getBill(b_id):
+	with SessionManager(Session) as session:
+		try:
+			bill = session.query(Bill).filter(Bill.id == b_id).one()
+			#cus = bill.customer 
+			
+			customer = bill.customer.first_name if bill.customer else 'Not Available'
+			dinetable = bill.dinetable.alias if bill.dinetable else 'Not Available'
+			employee = bill.employee.first_name if bill.employee else 'Not Available'
+			payment = bill.payment.p_type if bill.payment else 'Not Available'
+			vat = bill.vat.name if bill.vat else 'Not Available'
+			service_charge = bill.service_charge.name if bill.service_charge else 'Not Available' 
+
+			bill_description = bill.bill_description
+			on_site = bill.on_site
+			total_price = bill.total_price
+
+			items_orders = bill.items
+			list_item_orders = []
+			for item_order in items_orders:
+				list_item_orders.append(dict(item_id = item_order.item_id,
+											 bill_id = item_order.bill_id,
+											 quantity = item_order.quantity,
+											 order_price = item_order.order_price,
+											 order_time_stamp = item_order.order_time_stamp,
+											 item_name = item_order.item.name if item_order.item else 'Not Available',
+											 item_unit_price = item_order.item.unit_price if item_order.item else 0.0))
+
+
+			b = dict(customer=customer,
+					 dinetable=dinetable,
+					 employee=employee,
+					 payment=payment,
+					 vat=vat,
+					 service_charge=service_charge,
+					 bill_description=bill_description,
+					 on_site=on_site,
+					 total_price=total_price,
+					 item_orders = list_item_orders)
+
+			return jsonify(envelop(data=b, code=200))
+
+		except NoResultFound:
+			return jsonify(error_envelop(404, 'NoResultFound', 'Id : {0} Not Found'.format(b_id)))
+	return jsonify(error_envelop(400, 'UnknownError', 'Somethig went wrong'))
+
+
+
+@app.route('/api/v1/bills', methods=['GET'])
+def getBills():
+
+	'''Get all the bills '''
 	with SessionManager(Session) as session:
 
-		bill = session.query(Bill).filter(Bill.id == b_id).one()
-		customer = bill.customer.first_name
-		dinetable = bill.dinetable.alias
-		employee = bill.employee.first_name
-		payment = bill.payment.p_type
-		vat = bill.vat.name
-		service_charge = bill.service_charge.name 
+		sql_bills = session.query(Bill).order_by(Bill.id).all()
+		bills = []
+		for sql_bill in sql_bills:
+			customer = sql_bill.customer.first_name if sql_bill.customer else 'Not Available'
+			dinetable = sql_bill.dinetable.alias if sql_bill.dinetable else 'Not Available'
+			employee = sql_bill.employee.first_name if sql_bill.employee else 'Not Available'
+			payment = sql_bill.payment.p_type if sql_bill.payment else 'Not Available'
+			vat = sql_bill.vat.name if sql_bill.vat else 'Not Available'
+			service_charge = sql_bill.service_charge.name if sql_bill.service_charge else 'Not Available' 
 
-		bill_description = bill.bill_description
-		on_site = bill.on_site
-		total_price = bill.total_price
-
-		items_orders = bill.items
-		list_item_orders = []
-		for item_order in items_orders:
-			list_item_orders.append(dict(item_id = item_order.item_id,
-										 bill_id = item_order.bill_id,
-										 quantity = item_order.quantity,
-										 order_price = item_order.order_price,
-										 order_time_stamp = item_order.order_time_stamp,
-										 item_name = item_order.item.name,
-										 item_unit_price = item_order.item.unit_price))
-
-
-		b = dict(customer=customer,
+			bill_description = sql_bill.bill_description
+			on_site = sql_bill.on_site
+			total_price = sql_bill.total_price
+			b = dict(customer=customer,
 				 dinetable=dinetable,
 				 employee=employee,
 				 payment=payment,
@@ -111,11 +151,23 @@ def getBills(b_id):
 				 bill_description=bill_description,
 				 on_site=on_site,
 				 total_price=total_price,
-				 item_orders = list_item_orders)
-		return jsonify(envelop(data=b, code=200))
+				 id = sql_bill.id
+				 )
+			bills.append(b)
+		return jsonify(envelop(data=bills, code=200))
 
 
+@app.route('/api/v1/bills/<int:b_id>', methods=['DELETE'])
+def deleteBill(b_id):
+	with SessionManager(Session) as session:
+		try:
+			bill = session.query(Bill).filter(Bill.id==b_id).one()
+			session.delete(bill)
+			session.commit()
+			return jsonify(delete_envelop(code=200))
 
+		except NoResultFound: #causes when there is no requested id in the database
+			return jsonify(error_envelop(404, 'NoResultFound', ' Cannot delete!!Id : {0} Not Found'.format(b_id)))
 
-
-
+	
+	return jsonify(error_envelop(400, 'UnknownError', 'Somethig went wrong'))	
