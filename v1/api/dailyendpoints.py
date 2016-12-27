0237dea62ddc1f024fe669f5e4604bee16714055
Robus Gauli <robusgauli@gmail.com>
2016-12-27 18:07:49 +0545

    Asd

--- v1/api/dailyendpoints.py
+++ v1/api/dailyendpoints.py
@@ -26,11 +26,35 @@ def getDailyTopItem():
 	with SessionManager(Session) as session:
 		result = session.execute('select sum(quantity) , item_id from itemorder where order_time_stamp::date >= current_date group by item_id order by sum(quantity) desc limit 1')
 		
-		
+
 		
 		for row in result.fetchall():
 			item_id = row[1]
 			total_order = row[0]
 		return jsonify(item_id=item_id, total_orders=total_order)
 	
-		
+@app.route('/api/v1/daily/bill', methods=['GET'])
+def getDailyTotalBills():
+	with SessionManager(Session) as session:
+		result = session.execute('select count(id) from bills where bill_time_stamp::date = current_date limit 1')
+		for row in result.fetchall():
+			total_bills = row[0]
+		return jsonify(total_bills=total_bills)
+
+@app.route('/api/v1/daily/customers', methods=['GET'])
+def getTotalCustomers():
+	with SessionManager(Session) as session:
+		result = session.execute('select count(id) from customers limit 1')
+		for row in result.fetchall():
+			total_customers = row[0]
+		return jsonify(total_customers=total_customers)
+
+@app.route('/api/v1/daily/dinetable', methods=['GET'])
+def getTopDineTable():
+	with SessionManager(Session) as session:
+		result = session.execute('select dinetable_id, count(dinetable_id) from bills where bill_time_stamp::date=current_date group by dinetable_id order by count(dinetable_id) desc limit 1')
+		for row in result.fetchall():
+			dinetable_id = row[0]
+			total_bills = row[1]
+		return jsonify(dinetable_id=dinetable_id, total_bills=total_bills)
+
